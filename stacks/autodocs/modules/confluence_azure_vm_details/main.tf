e24598fdd5637b6679603a2e488466d17a85c2ae
Konrad Ferdinand Heimel <konrad.heimel@nagarro.com>
2024-05-31 12:00:51 +0200

    chore: :wastebasket: flag for table of contents

--- stacks/autodocs/modules/confluence_azure_vm_details/main.tf
+++ stacks/autodocs/modules/confluence_azure_vm_details/main.tf
@@ -9,19 +9,20 @@ locals {
     "production" = "🚀",
     "n/a"        = "❓",
   }
-  service_class     = try(var.vm_details.tags["Service class"], try(var.vm_details.tags["Service Class"], "N/A"))
+  service_class       = try(var.vm_details.tags["Service class"], try(var.vm_details.tags["Service Class"], "N/A"))
   service_class_emoji = lookup(local.service_class_emojis, lower(local.service_class), "🤷")
 
-  network_interfaces_html = templatefile("${path.module}/network_interfaces.tpl", {
-    network_interfaces = { for interface in var.vm_details.network_interfaces : interface.id => interface }
+  network_interfaces_html = templatefile("${path.module}/network_interfaces.html", {
+    network_interfaces = {for interface in var.vm_details.network_interfaces : interface.id => interface}
   })
 
-  content = templatefile("${path.module}/azure_vm_template.tpl", {
-    last_generated_date = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
-    vm                      = var.vm_details,
-    network_interfaces_html = local.network_interfaces_html
-    service_class_emoji     = local.service_class_emoji,
-    service_class           = local.service_class,
+  content = templatefile("${path.module}/azure_vm_template.html", {
+    last_generated_date       = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+    include_table_of_contents = false
+    vm                        = var.vm_details,
+    network_interfaces_html   = local.network_interfaces_html
+    service_class_emoji       = local.service_class_emoji,
+    service_class             = local.service_class,
   })
 
 }
